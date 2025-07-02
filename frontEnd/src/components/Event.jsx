0d33990b6d3ea49d919277011d17a8e7955f3eb0
AbhishekPatil1310 <ap7497493@gmail.com>
2025-07-02 21:16:50 +0530

    Update Event.jsx

--- frontEnd/src/components/Event.jsx
+++ frontEnd/src/components/Event.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import '../style/Event.css';
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 function EventList() {
   const [events, setEvents] = useState([]);
